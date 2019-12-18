3ac2e33a516fa83d172b6e98af34e0179bcf91b2
Michael Tanczos <tanczosm@eastonsd.org>
2019-12-17 22:55:37 -0500

    Starting to add file upload capabilities to forms

--- ADSBackend/Controllers/AnswerGroupController.cs
+++ ADSBackend/Controllers/AnswerGroupController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -108,7 +110,32 @@ namespace Scholarships.Controllers
             };
         }
 
-            private bool AnswerSetExists(int id)
+
+        public async Task<ActionResult> Upload(IEnumerable<IFormFile> files, int questionid, int answersetid)
+        {
+            long size = files.Sum(f => f.Length);
+
+            // full path to file in temp location
+            var filePath = Path.GetTempFileName();
+
+            foreach (var formFile in files)
+            {
+                if (formFile.Length > 0)
+                {
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await formFile.CopyToAsync(stream);
+                    }
+                }
+            }
+
+            // process uploaded files
+            // Don't rely on or trust the FileName property without validation.
+
+            return Ok();
+        }
+
+        private bool AnswerSetExists(int id)
         {
             return _context.AnswerSet.Any(e => e.AnswerSetId == id);
         }
--- ADSBackend/Models/Forms/Answer.cs
+++ ADSBackend/Models/Forms/Answer.cs
@@ -17,7 +17,7 @@ namespace Scholarships.Models.Forms
         public Question Question { get; set; }
         public string Response { get; set; }  // For free response, stores answer as a string
         public int QuestionOptionId { get; set; }  // For multiple choice, stores answer as the option id
-        public DateTime DateTime { get; set; }  // For datetime, stores answer as a Date
+        public DateTime DateTime { get; set; } = DateTime.Now;  // For datetime, stores answer as a Date
         public string Config { get; set; }  // Any extraneous JSON-encoded data for this answer
 
         [NotMapped]
